f45bdc16a975902c7c90bec10d8974b9158daa2d
Chris <filmee24@gmail.com>
2023-07-20 18:50:58 +0200

    feat: Add ConditionView

---
+++ samples/NodeEditor.Base/Core/Components/ViewModels/ConditionView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NodeEditorDemo.Core.Components.ViewModels;
+
+public partial class ConditionView : UserControl
+{
+    public ConditionView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+
