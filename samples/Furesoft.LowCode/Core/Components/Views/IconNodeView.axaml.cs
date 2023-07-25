4eedfa688ff3e10b0114e25de73315a7d6e77515
Chris <filmee24@gmail.com>
2023-07-25 23:39:48 +0200

    feat: Add IconNodeView

---
+++ samples/Furesoft.LowCode/Core/Components/Views/IconNodeView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Furesoft.LowCode.Core.Components.Views;
+
+public partial class IconNodeView : UserControl
+{
+    public IconNodeView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+
