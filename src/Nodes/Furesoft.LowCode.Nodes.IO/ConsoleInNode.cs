e1b997955f26e699f3a0bfaf8513af98ca6a6aa1
Chris <filmee24@gmail.com>
2023-07-25 23:50:44 +0200

    feat: Add Icons

--- src/Nodes/Furesoft.LowCode.Nodes.IO/ConsoleInNode.cs
+++ src/Nodes/Furesoft.LowCode.Nodes.IO/ConsoleInNode.cs
@@ -1,13 +1,15 @@
 ﻿using System.ComponentModel;
 using System.Runtime.Serialization;
 using Furesoft.LowCode.Core;
+using Furesoft.LowCode.Core.Components.Views;
 using Furesoft.LowCode.Core.NodeBuilding;
 using NodeEditor.Model;
 
 namespace Furesoft.LowCode.Nodes.IO;
 
-[NodeCategory("IO")]
 [Description("Read a value from the console")]
+[NodeCategory("IO")]
+[NodeView(typeof(IconNodeView), "M67.5375 87.1125 5.6625 148.9875A12.5 12.5 0 1023.3375 166.6625L94.05 95.95A12.5 12.5 0 0094.05 78.275L23.3375 7.5625A12.5 12.5 0 005.6625 25.2375L67.5375 87.1125zM87 173 190 172A12.5 12.5 0 00185 143L93 144A12.5 12.5 0 0086 173z")]
 public class ConsoleInNode : VisualNode
 {
     [Description("The input from the console")]
--- src/Nodes/Furesoft.LowCode.Nodes.IO/ConsoleOutNode.cs
+++ src/Nodes/Furesoft.LowCode.Nodes.IO/ConsoleOutNode.cs
@@ -1,13 +1,15 @@
 ﻿using System.ComponentModel;
 using System.Runtime.Serialization;
 using Furesoft.LowCode.Core;
+using Furesoft.LowCode.Core.Components.Views;
 using Furesoft.LowCode.Core.NodeBuilding;
 using NodeEditor.Model;
 
 namespace Furesoft.LowCode.Nodes.IO;
 
-[NodeCategory("IO")]
 [Description("Write a text to the console")]
+[NodeCategory("IO")]
+[NodeView(typeof(IconNodeView), "M0 0 42 0 42 36 0 36 0 0ZM3 6 3 33 39 33 39 6 3 6ZM6.75 11 11.5 11 16.25 17.5 11.5 24 6.75 24 11.5 17.5 6.75 11Z")]
 public class ConsoleOutNode : VisualNode
 {
     [Description("The text to display")]
