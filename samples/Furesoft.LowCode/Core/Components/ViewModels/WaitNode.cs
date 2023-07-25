10dcc6a962f0b31928400027adb2ccff26692e90
Chris <filmee24@gmail.com>
2023-07-26 00:16:18 +0200

    refactor: Cleanup

--- samples/Furesoft.LowCode/Core/Components/ViewModels/WaitNode.cs
+++ samples/Furesoft.LowCode/Core/Components/ViewModels/WaitNode.cs
@@ -8,7 +8,7 @@ using NodeEditor.Model;
 namespace Furesoft.LowCode.Core.Components.ViewModels;
 
 [DataContract(IsReference = true)]
-[NodeView(typeof(WaitView))]
+[NodeView(typeof(IconNodeView), "M0 0H12V6H12V6L8 10 12 14V14H12V20H0V14H0V14L4 10 0 6V6H0V0M10 14.5 6 10.5 2 14.5V18H10V14.5M6 9.5 10 5.5V2H2V5.5L6 9.5M4 4H8V4.75L6 6.75 4 4.75V4Z")]
 [NodeCategory]
 [Description("Wait for the specified time")]
 public class WaitNode : VisualNode
