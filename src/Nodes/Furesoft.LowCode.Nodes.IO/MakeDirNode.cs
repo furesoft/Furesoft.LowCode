1ceb104d3d8b58fb1f7674818f9905763087d4a5
Chris <filmee24@gmail.com>
2023-07-28 19:49:01 +0200

    feat: Add Make Directory Node

---
+++ src/Nodes/Furesoft.LowCode.Nodes.IO/MakeDirNode.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
+using Furesoft.LowCode.Designer.Core;
+using Furesoft.LowCode.Designer.Core.NodeBuilding;
+using Furesoft.LowCode.Editor.Model;
+
+namespace Furesoft.LowCode.Nodes.IO;
+
+[Description("Create a directory")]
+[NodeCategory("IO")]
+public class MakeDirNode : VisualNode
+{
+    [DataMember(IsRequired = false, EmitDefaultValue = false)]
+    public string Path { get; set; }
+    public MakeDirNode() : base("Make Directory")
+    {
+    }
+    
+    [Pin("Flow Output", PinAlignment.Bottom)]
+    public IOutputPin OutputPin { get; set; }
+    
+    [Pin("Flow Input", PinAlignment.Top)]
+    public IInputPin InputPin { get; set; }
+
+    public override Task Execute()
+    {
+        Directory.CreateDirectory(Evaluate<string>(Path));
+
+        return ContinueWith(OutputPin);
+    }
+}
