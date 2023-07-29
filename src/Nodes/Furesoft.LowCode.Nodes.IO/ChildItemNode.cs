a589ae05f37abd95c2419890e960ea5f5ed94acb
IXLLEGACYIXL <107197024+IXLLEGACYIXL@users.noreply.github.com>
2023-07-29 17:40:37 +0200

    add powershell extras

--- src/Nodes/Furesoft.LowCode.Nodes.IO/ChildItemNode.cs
+++ src/Nodes/Furesoft.LowCode.Nodes.IO/ChildItemNode.cs
@@ -9,6 +9,7 @@ using Furesoft.LowCode.Designer.Core;
 using Microsoft.CodeAnalysis;
 using Newtonsoft.Json;
 using System.IO.Enumeration;
+using System.Linq;
 
 namespace Furesoft.LowCode.Nodes.IO;
 [Description("Get Items in a Folder")]
@@ -30,10 +31,23 @@ internal class ChildItemNode : VisualNode
     [Description("Which Items of a Folder should be selected")]
     [DataMember(IsRequired = false, EmitDefaultValue = false)]
     public ItemType ItemType { get; set; } = ItemType.All;
-    
+    [Description("Should the symlink be followed")]
+    [DataMember(IsRequired = false, EmitDefaultValue = false)]
+    public bool FollowSymlink { get; set; }
+
+    [Description("Should only the Names be listed")]
+    [DataMember(IsRequired = false, EmitDefaultValue = false)]
+    public bool OnlyName { get; set; }
+
+    [Description("Filters all Entries if they have that flag")]
+    [DataMember(IsRequired = false, EmitDefaultValue = false)]
+    public BindingList<FileAttributes> RequiredFlags { get; set; } = new();
+
     [Description("Where to store the folder content")]
     [DataMember(IsRequired = false, EmitDefaultValue = false)]
     public string OutVariable { get; set; }
+
+
 
     [Pin("Flow Input", PinAlignment.Top)]
     public IInputPin InputPin { get; set; }
@@ -47,13 +61,26 @@ internal class ChildItemNode : VisualNode
     
     public override Task Execute(CancellationToken cancellationToken)
     {
+
         var folderPath = Evaluate<string>(FolderPath);
         var searchOption = IsRecurse ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
-        DirectoryInfo dirInfo = new(folderPath);
 
         if (string.IsNullOrEmpty(SearchPattern))
         {
             SearchPattern = "*";
+        }
+
+        DirectoryInfo dirInfo = new DirectoryInfo(folderPath);
+        if (FollowSymlink)
+        {
+            if (dirInfo.ResolveLinkTarget(true) is DirectoryInfo resolvedDir)
+            {
+                dirInfo = resolvedDir;
+            }
+            else
+            {
+                throw new Exception("Resolved Symlink doesn't link to a Directory");
+            }
         }
 
         var fileInfos = ItemType switch
@@ -62,7 +89,18 @@ internal class ChildItemNode : VisualNode
             ItemType.Folder => dirInfo.GetDirectories(SearchPattern, searchOption),
             ItemType.All => dirInfo.GetFileSystemInfos(SearchPattern, searchOption)
         };
-        SetOutVariable(OutVariable, fileInfos);
+       // if (!IncludeHidden)
+        {
+          //  fileInfos = file
+        }
+        if (OnlyName)
+        {
+            SetOutVariable(OutVariable, fileInfos.Select(x => x.Name));
+        }
+        else
+        {
+            SetOutVariable(OutVariable, fileInfos);
+        }
 
         return ContinueWith(OutputPin, cancellationToken: cancellationToken);
     }
