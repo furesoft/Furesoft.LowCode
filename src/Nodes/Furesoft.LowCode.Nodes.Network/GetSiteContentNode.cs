012dc29704a4bb0d71a766aca85900997c1fa131
Chris <filmee24@gmail.com>
2023-07-30 19:22:38 +0200

    feat: Add GetSiteContentNode

---
+++ src/Nodes/Furesoft.LowCode.Nodes.Network/GetSiteContentNode.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+
+namespace Furesoft.LowCode.Nodes.Network;
+
+[Description("Gets the content of a website")]
+[DataContract(IsReference = true)]
+[NodeCategory]
+public class GetSiteContentNode : VisualNode
+{
+    private string _url;
+    private string _outputVariable;
+
+    [Pin("Input Flow", PinAlignment.Top)]
+    public IInputPin InputPin { get; set; }
+    
+    [Pin("Output Flow", PinAlignment.Top)]
+    public IOutputPin OutPin { get; set; }
+
+    public string URL
+    {
+        get => _url;
+        set => SetProperty(ref _url, value);
+    }
+
+    public string OutputVariable
+    {
+        get => _outputVariable;
+        set => SetProperty(ref _outputVariable, value);
+    }
+
+    public GetSiteContentNode() : base("Get Site Content")
+    {
+    }
+
+    public override async Task Execute(CancellationToken cancellationToken)
+    {
+        var client = new HttpClient();
+        var content = await client.GetStringAsync(URL, cancellationToken);
+        
+        SetOutVariable(OutputVariable, content);
+    }
+}
