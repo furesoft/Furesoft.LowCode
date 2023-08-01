2aba99b43cbdb9ff2a877597f71d94051a95b658
Chris <filmee24@gmail.com>
2023-08-01 19:59:51 +0200

    refactor: Cleanup

--- src/Core/Furesoft.LowCode.Designer/Core/InputOutputNode.cs
+++ src/Core/Furesoft.LowCode.Designer/Core/InputOutputNode.cs
@@ -1,12 +1,10 @@
 ﻿namespace Furesoft.LowCode.Designer.Core;
 
-public abstract class InputOutputNode : EmptyNode
+public abstract class InputOutputNode : OutputNode
 {
     protected InputOutputNode(string label) : base(label)
     {
     }
 
     [Pin("Flow", PinAlignment.Top)] public IInputPin InputPin { get; set; }
-
-    [Pin("Flow", PinAlignment.Bottom)] public IOutputPin OutputPin { get; set; }
 }
