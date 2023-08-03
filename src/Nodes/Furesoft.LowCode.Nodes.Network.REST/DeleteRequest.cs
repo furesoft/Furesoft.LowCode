e7240fd9872f5ba7d25174d58964e9a1629b2317
Chris <filmee24@gmail.com>
2023-08-03 16:06:41 +0200

    Add More REST Nodes

---
+++ src/Nodes/Furesoft.LowCode.Nodes.Network.REST/DeleteRequest.cs
@@ -0,0 +1,19 @@
+﻿using Furesoft.LowCode.Designer.Core;
+
+namespace Furesoft.LowCode.Nodes.Network.REST;
+
+public class DeleteRequest : RestBaseNode, IOutVariableProvider
+{
+    public string Content { get; set; }
+    public string OutVariable { get; set; }
+    public DeleteRequest() : base("DELETE")
+    {
+    }
+
+    public override async Task Invoke(CancellationToken cancellationToken, string evaluatedUrl)
+    {
+        var result = await client.DeleteAsync(evaluatedUrl, cancellationToken);
+        
+        SetOutVariable(OutVariable, result);
+    }
+}
---
+++ src/Nodes/Furesoft.LowCode.Nodes.Network.REST/PutRequest.cs
@@ -0,0 +1,19 @@
+﻿using Furesoft.LowCode.Designer.Core;
+
+namespace Furesoft.LowCode.Nodes.Network.REST;
+
+public class PutRequest : RestBaseNode, IOutVariableProvider
+{
+    public string Content { get; set; }
+    public string OutVariable { get; set; }
+    public PutRequest() : base("PUT")
+    {
+    }
+
+    public override async Task Invoke(CancellationToken cancellationToken, string evaluatedUrl)
+    {
+        var result = await client.PutAsync(evaluatedUrl, new StringContent(Content), cancellationToken);
+        
+        SetOutVariable(OutVariable, result);
+    }
+}
