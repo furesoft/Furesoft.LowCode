1c6ef04e24e6781ac779adfee41a2e1a92a4618a
Chris <filmee24@gmail.com>
2023-08-05 16:32:25 +0200

    feat: Add More Browser Nodes

---
+++ src/Nodes/Furesoft.LowCode.Nodes.RPA.Web/CloseBrowserNode.cs
@@ -0,0 +1,13 @@
+﻿namespace Furesoft.LowCode.Nodes.RPA.Web;
+
+public class CloseBrowserNode : WebNode
+{
+    public CloseBrowserNode() : base("Close Browser")
+    {
+    }
+
+    protected override async Task Invoke(CancellationToken cancellationToken)
+    {
+        await GetBrowser().CloseAsync();
+    }
+}
---
+++ src/Nodes/Furesoft.LowCode.Nodes.RPA.Web/NewTabNode.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace Furesoft.LowCode.Nodes.RPA.Web;
+
+public class NewTabNode : WebNode
+{
+    [DataMember(EmitDefaultValue = false)]
+    public string URL { get; set; }
+
+    public NewTabNode() : base("New Tab")
+    {
+
+    }
+
+    protected override async Task Invoke(CancellationToken cancellationToken)
+    {
+        var page = await GetBrowser().NewPageAsync();
+        await page.GoToAsync(Evaluate<string>(URL));
+        
+        DefineConstant(PageVariableName, page);
+    }
+}
