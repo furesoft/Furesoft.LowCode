e0189c8eed3dca7dae195fae89a5a2a12ab2d1e2
Chris <filmee24@gmail.com>
2023-08-05 13:35:03 +0200

    refactor: Cleanup

--- src/Nodes/Furesoft.LowCode.Nodes.IO/Filesystem/CopyItemNode.cs
+++ src/Nodes/Furesoft.LowCode.Nodes.IO/Filesystem/CopyItemNode.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Furesoft.LowCode.Nodes.IO.Filesystem;
 
-namespace Furesoft.LowCode.Nodes.IO.Filesystem;
 [Description("Copies Filesystem Items")]
 [NodeCategory("IO/FileSystem")]
 public class CopyItemNode : InputOutputNode
 {
     [Description("The Path of the items to be copied")]
-    [DataMember(IsRequired = false, EmitDefaultValue = false)]
     public string SourcePath { get; set; }
+    
     [Description("The Path to the to the destination of the items")]
-    [DataMember(IsRequired = false, EmitDefaultValue = false)]
     public string DestinationPath { get; set; }
     public CopyItemNode() : base("CopyItem")
     {
@@ -24,6 +18,7 @@ public class CopyItemNode : InputOutputNode
         var sourcePath = Evaluate<string>(SourcePath);
         var destinationPath = Evaluate<string>(DestinationPath);
         var folderDestinationPath = Path.GetDirectoryName(destinationPath);
+        
         if (!Directory.Exists(folderDestinationPath))
         {
             Directory.CreateDirectory(folderDestinationPath);
@@ -36,6 +31,7 @@ public class CopyItemNode : InputOutputNode
         else if (Directory.Exists(sourcePath))
         {
             var files = Directory.GetFiles(sourcePath);
+            
             foreach (string file in files)
             {
                 var fileName = Path.GetFileName(file);
@@ -44,6 +40,7 @@ public class CopyItemNode : InputOutputNode
                 File.Copy(file, destination, true);
             }
         }
+        
         return ContinueWith(OutputPin, cancellationToken: cancellationToken);
     }
 }
