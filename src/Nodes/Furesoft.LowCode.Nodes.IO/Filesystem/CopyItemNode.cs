efc89bdb61801b3add241f2cc24b234cb17750db
IXLLEGACYIXL <107197024+IXLLEGACYIXL@users.noreply.github.com>
2023-08-10 17:25:54 +0200

    Add recursion to Copy item

--- src/Nodes/Furesoft.LowCode.Nodes.IO/Filesystem/CopyItemNode.cs
+++ src/Nodes/Furesoft.LowCode.Nodes.IO/Filesystem/CopyItemNode.cs
@@ -7,10 +7,13 @@ public class CopyItemNode : InputOutputNode
     [DataMember(EmitDefaultValue = false)]
     [Description("The Path of the items to be copied")]
     public string SourcePath { get; set; }
-    
     [Description("The Path to the to the destination of the items")]
     [DataMember(EmitDefaultValue = false)]
     public string DestinationPath { get; set; }
+    [Description("Should a folder be copied recursively. No effect on file copies.")]
+    [DataMember(EmitDefaultValue = false)]
+    public bool IsRecursive { get; set; }
+
     public CopyItemNode() : base("CopyItem")
     {
     }
@@ -20,7 +23,6 @@ public class CopyItemNode : InputOutputNode
         var sourcePath = Evaluate<string>(SourcePath);
         var destinationPath = Evaluate<string>(DestinationPath);
         var folderDestinationPath = Path.GetDirectoryName(destinationPath);
-        
         if (!Directory.Exists(folderDestinationPath))
         {
             Directory.CreateDirectory(folderDestinationPath);
@@ -32,17 +34,48 @@ public class CopyItemNode : InputOutputNode
         }
         else if (Directory.Exists(sourcePath))
         {
-            var files = Directory.GetFiles(sourcePath);
-            
-            foreach (string file in files)
+            if (IsRecursive)
             {
-                var fileName = Path.GetFileName(file);
-                var destination = Path.Combine(folderDestinationPath, fileName);
+                CopyFilesRecursively(sourcePath, destinationPath);
+            }
+            else
+            {
+                var files = Directory.GetFiles(sourcePath);
+                foreach (string file in files)
+                {
+                    var fileName = Path.GetFileName(file);
+                    var destination = Path.Combine(folderDestinationPath, fileName);
+                    File.Copy(file, destination, true);
+                }
+            }
 
-                File.Copy(file, destination, true);
+        }
+        return ContinueWith(OutputPin, cancellationToken: cancellationToken);
+    }
+    public static void CopyFilesRecursively(string sourcePath, string destinationPath)
+    {
+        if (Directory.Exists(sourcePath))
+        {
+            if (!Directory.Exists(destinationPath))
+            {
+                Directory.CreateDirectory(destinationPath);
+            }
+
+            var entries = Directory.GetFileSystemEntries(sourcePath);
+
+            foreach (var entry in entries)
+            {
+                var sourceEntryPath = Path.GetFullPath(entry);
+                var destinationEntryPath = Path.Combine(destinationPath, Path.GetFileName(entry));
+                if (Directory.Exists(sourceEntryPath))
+                {
+                    CopyFilesRecursively(sourceEntryPath, destinationEntryPath);
+                }
+                else
+                {
+                    File.Copy(sourceEntryPath, destinationEntryPath, true);
+                }
             }
         }
-        
-        return ContinueWith(OutputPin, cancellationToken: cancellationToken);
     }
 }
