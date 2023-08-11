99143ae1e19bb82b390790805acbce309b04314f
Chris <furesoft@users.noreply.github.com>
2023-08-11 06:38:42 +0200

    feat: Add Queue Nodes (#7)

---
+++ src/Nodes/Furesoft.LowCode.Nodes.Queue/DequeueNode.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace Furesoft.LowCode.Nodes.Queue;
+
+[Description("Get data from queue")]
+public class DequeueNode : QueueBaseNode, IOutVariableProvider
+{
+    public DequeueNode() : base("Get From Queue")
+    {
+    }
+
+    [DataMember] public string OutVariable { get; set; }
+
+
+    public override Task Invoke(CancellationToken cancellationToken)
+    {
+        var value = QueueManager.Instance.Dequeue<object>(Queue);
+        SetOutVariable(OutVariable, value);
+
+        return Task.CompletedTask;
+    }
+}
