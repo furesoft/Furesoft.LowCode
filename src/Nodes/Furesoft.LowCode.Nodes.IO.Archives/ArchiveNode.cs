af24470c293dd91440e24750e546356f728ac860
Chris <filmee24@gmail.com>
2023-08-13 14:51:05 +0200

    feat: Add Archive Node

---
+++ src/Nodes/Furesoft.LowCode.Nodes.IO.Archives/ArchiveNode.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.Serialization;
+using Furesoft.LowCode.Attributes;
+using Furesoft.LowCode.Evaluation;
+using SharpCompress.Common;
+using SharpCompress.Writers;
+
+namespace Furesoft.LowCode.Nodes.IO.Archives;
+
+[NodeCategory("IO/FileSystem")]
+public class ArchiveNode : InputOutputNode
+{
+    public ArchiveNode() : base("Compress Directory To Archive")
+    {
+    }
+
+    [DataMember(EmitDefaultValue = false)]
+    public Evaluatable<string> Path { get; set; }
+    
+    [DataMember(EmitDefaultValue = false)]
+    public Evaluatable<string> OutputFilename { get; set; }
+    
+    [DataMember(EmitDefaultValue = false)]
+    public ArchiveType Type { get; set; }
+
+    [DataMember(EmitDefaultValue = false)] public string SearchPattern { get; set; } = "*";
+
+    public override async Task Execute(CancellationToken cancellationToken)
+    {
+        await using (var zip = File.OpenWrite(Evaluate(OutputFilename)))
+        using (var zipWriter = WriterFactory.Open(zip, Type, CompressionType.Deflate))
+        {
+            zipWriter.WriteAll(Evaluate(Path), SearchPattern, SearchOption);
+        }
+
+        await ContinueWith(OutputPin, cancellationToken: cancellationToken);
+    }
+
+    [DataMember(EmitDefaultValue = false)]
+    public SearchOption SearchOption { get; set; }
+}
