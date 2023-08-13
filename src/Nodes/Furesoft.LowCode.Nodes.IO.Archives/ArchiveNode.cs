9934641905238aa9a903335df5c964f90bb2f788
Chris <filmee24@gmail.com>
2023-08-13 17:23:24 +0200

    feat: Add Extract Node

--- src/Nodes/Furesoft.LowCode.Nodes.IO.Archives/ArchiveNode.cs
+++ src/Nodes/Furesoft.LowCode.Nodes.IO.Archives/ArchiveNode.cs
@@ -1,12 +1,14 @@
 ﻿using System.Runtime.Serialization;
 using Furesoft.LowCode.Attributes;
 using Furesoft.LowCode.Evaluation;
+using Furesoft.LowCode.NodeViews;
 using SharpCompress.Common;
 using SharpCompress.Writers;
 
 namespace Furesoft.LowCode.Nodes.IO.Archives;
 
 [NodeCategory("IO/FileSystem")]
+[NodeView(typeof(IconNodeView), "M0 0H18V4H0V0M1 18V5H17V18H1M11 11V8H7V11H4L9 16 14 11H11Z")]
 public class ArchiveNode : InputOutputNode
 {
     public ArchiveNode() : base("Compress Directory To Archive")
---
+++ src/Nodes/Furesoft.LowCode.Nodes.IO.Archives/ExtractNode.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.Serialization;
+using Furesoft.LowCode.Attributes;
+using Furesoft.LowCode.Evaluation;
+using Furesoft.LowCode.NodeViews;
+using SharpCompress.Readers;
+
+namespace Furesoft.LowCode.Nodes.IO.Archives;
+
+[NodeCategory("IO/FileSystem")]
+[NodeView(typeof(IconNodeView), "M1 18H17V5H1M11 12V15H7V12H4L9 7 14 12M0 0H18V4H0")]
+public class ExtractNode : InputOutputNode
+{
+    
+    [DataMember(EmitDefaultValue = false)]
+    public Evaluatable<string> ArchiveFilename { get; set; }
+    
+    [DataMember(EmitDefaultValue = false)]
+    public Evaluatable<string> OutputDirectory { get; set; }
+    
+    public ExtractNode() : base("Extract Archive")
+    {
+    }
+
+    public override async Task Execute(CancellationToken cancellationToken)
+    {
+        await using Stream stream = File.OpenRead(ArchiveFilename);
+        var reader = ReaderFactory.Open(stream);
+        
+        while (reader.MoveToNextEntry())
+        {
+            if (reader.Entry.IsDirectory)
+            {
+                continue;
+            }
+
+            Console.WriteLine(reader.Entry.Key);
+            reader.WriteEntryToDirectory(OutputDirectory, new() { ExtractFullPath = true, Overwrite = true });
+        }
+
+        await ContinueWith(OutputPin, cancellationToken: cancellationToken);
+    }
+}
