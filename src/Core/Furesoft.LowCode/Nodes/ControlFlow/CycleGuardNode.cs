30b2f7b704d1755c917cab23698ad76ba4368f70
Chris <filmee24@gmail.com>
2023-08-19 15:28:37 +0200

    refactor: Cleanup

--- src/Core/Furesoft.LowCode/Nodes/ControlFlow/CycleGuardNode.cs
+++ src/Core/Furesoft.LowCode/Nodes/ControlFlow/CycleGuardNode.cs
@@ -10,9 +10,9 @@ namespace Furesoft.LowCode.Nodes.ControlFlow;
 [Description("Breaks an endless cycle by counting the iterations")]
 [NodeCategory("Control Flow")]
 [NodeView(typeof(IconNodeView), "M12 0C8.249 0 3.725.861 0 2.755 0 6.845-.051 17.037 12 24 24.051 17.037 24 6.845 24 2.755 20.275.861 15.751 0 12 0zm-.106 15.429L6.857 9.612c.331-.239 1.75-1.143 2.794.042l2.187 2.588c.009-.001 5.801-5.948 5.815-5.938.246-.22.694-.503 1.204-.101l-6.963 9.226z")]
-public class CycleGuard : InputOutputNode
+public class CycleGuardNode : InputOutputNode
 {
-    public CycleGuard() : base("Cycle Guard")
+    public CycleGuardNode() : base("Cycle Guard")
     {
     }
 
