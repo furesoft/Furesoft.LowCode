f3ca4a00978c1c446e9befeb1fec795128c552fc
Chris <filmee24@gmail.com>
2023-08-22 10:26:28 +0200

    feat: Add CSV Reader And Writer Nodes

--- src/Core/Furesoft.LowCode/Nodes/Data/DataTable/DataTableNode.cs
+++ src/Core/Furesoft.LowCode/Nodes/Data/DataTable/DataTableNode.cs
@@ -1,10 +1,27 @@
-﻿using NiL.JS.Extensions;
+﻿using System.Runtime.Serialization;
+using Furesoft.LowCode.Evaluation;
+using NiL.JS.Core;
+using NiL.JS.Extensions;
 
 namespace Furesoft.LowCode.Nodes.Data.DataTable;
 
 public abstract class DataTableNode : InputOutputNode
 {
-    public string TableName { get; set; }
+    private string _tableName;
+
+    [DataMember(EmitDefaultValue = false)]
+    public string TableName
+    {
+        get => _tableName;
+        set
+        {
+            SetProperty(ref _tableName, value);
+            Description = $"{Label} '{TableName}'";
+        }
+    }
+
+    [DataMember(EmitDefaultValue = false)]
+    public Evaluatable<string> Path { get; set; }
     public DataTableNode(string label) : base(label)
     {
     }
@@ -18,7 +35,14 @@ public abstract class DataTableNode : InputOutputNode
 
     protected System.Data.DataTable GetTable()
     {
-       return Context.GetVariable(TableName).As<System.Data.DataTable>();
+        var value = Context.GetVariable(TableName);
+
+        return value != JSValue.NotExists ? value.As<System.Data.DataTable>() : null;
+    }
+
+    protected void SetTable(System.Data.DataTable table)
+    {
+        DefineConstant(TableName, table);
     }
 
     protected abstract Task Invoke(CancellationToken cancellationToken);
