e6fb9ae04c496486c9cb02b376e36a24ff8b0117
Chris <filmee24@gmail.com>
2023-08-22 15:44:04 +0200

    feat: Add Ability To Do Something On Load And Exit

--- src/Core/Furesoft.LowCode/Designer/ViewModels/ViewModelBase.cs
+++ src/Core/Furesoft.LowCode/Designer/ViewModels/ViewModelBase.cs
@@ -6,4 +6,28 @@ namespace Furesoft.LowCode.Designer.ViewModels;
 public class ViewModelBase : ObservableObject
 {
     [Browsable(false)] public IDrawingNode Drawing { get; set; }
+
+    public event Action OnRequestClose;
+
+    public bool IsLoaded { get; set; }
+
+
+    public void Load()
+    {
+        if (!IsLoaded)
+        {
+            IsLoaded = true;
+
+            OnLoad();
+        }
+    }
+
+    protected virtual void OnLoad()
+    {
+    }
+
+    protected void RequestClose()
+    {
+        OnRequestClose?.Invoke();
+    }
 }
