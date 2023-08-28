6e35f4a86bd9fb145e8cc481336d6d01ec907ed5
Chris <furesoft@users.noreply.github.com>
2023-08-28 11:54:56 +0200

    Flc 59 (#19)
    * feat: Add RemoveColumnsNode
    
    * feat: Add Naive Transformer
    
    * feat: Use Evaluated JavaScript As Value

--- src/Core/Furesoft.LowCode/Nodes/Data/DataTable/DataTableNode.cs
+++ src/Core/Furesoft.LowCode/Nodes/Data/DataTable/DataTableNode.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Data;
 using System.Runtime.Serialization;
 using Furesoft.LowCode.Evaluation;
 using Furesoft.LowCode.Nodes.Data.DataTable.Core;
@@ -33,15 +34,62 @@ public abstract class DataTableNode : InputOutputNode, IPipeable
             }
         }
     }
+    //@Firstname - @Lastname -> Fullname
+
+    public BindingList<string> Transformers { get; set; } = new();
+
 
     [DataMember(EmitDefaultValue = false)] public Evaluatable<string> Path { get; set; }
     public object PipeVariable { get; set; }
+
+    private void ApplyTransformers()
+    {
+        var table = GetTable();
+
+        foreach (var transformer in Transformers)
+        {
+            var parsedTransformer = TransformerRule.Parse(transformer);
+
+            if (parsedTransformer is NewColumnRule newColumnTransformer)
+            {
+                var column = new DataColumn(newColumnTransformer.ColumnName);
+
+                if (!table.Columns.Contains(column.ColumnName))
+                {
+                    continue;
+                }
+
+                CalculateColumnValues(table, column, newColumnTransformer);
+
+                table.Columns.Add(column);
+            }
+        }
+    }
+
+    private void CalculateColumnValues(System.Data.DataTable table, DataColumn column, NewColumnRule newColumnTransformer)
+    {
+        foreach (DataRow row in table.Rows)
+        {
+            var context = new Context(Context.GlobalContext);
+
+            for (var index = 0; index < row.Table.Columns.Count; index++)
+            {
+                var c = row.Table.Columns[index];
+
+                context.DefineConstant(c.ColumnName, row.ItemArray[index].ToString());
+            }
+
+            row.SetField(column, context.Eval(newColumnTransformer.Pattern));
+        }
+    }
 
     public override async Task Execute(CancellationToken cancellationToken)
     {
         ApplyPipe<System.Data.DataTable>();
 
         await Invoke(cancellationToken);
+
+        ApplyTransformers();
 
         await ContinueWith(OutputPin, cancellationToken);
     }
@@ -74,4 +122,35 @@ public abstract class DataTableNode : InputOutputNode, IPipeable
     }
 
     protected abstract Task Invoke(CancellationToken cancellationToken);
+
+    private class TransformerRule
+    {
+        public static TransformerRule Parse(string src)
+        {
+            if (src.Contains(':'))
+            {
+                return NewColumnRule.Parse(src);
+            }
+
+            return null;
+        }
+    }
+
+    //Fullname: @Firstname - @Lastname
+    private class NewColumnRule : TransformerRule
+    {
+        public string ColumnName { get; set; }
+        public string Pattern { get; set; }
+
+        public new static NewColumnRule Parse(string src)
+        {
+            var result = new NewColumnRule();
+
+            var spl = src.Split(":", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            result.ColumnName = spl[0];
+            result.Pattern = spl[1];
+
+            return result;
+        }
+    }
 }
---
+++ src/Core/Furesoft.LowCode/Nodes/Data/DataTable/RemoveColumnsNode.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using Furesoft.LowCode.Attributes;
+using Furesoft.LowCode.Nodes.Data.DataTable.Core;
+
+namespace Furesoft.LowCode.Nodes.Data.DataTable;
+
+[NodeCategory("Data")]
+public class RemoveColumnsNode : DataTableNode
+{
+    public BindingList<string> Columns { get; set; }
+
+    public RemoveColumnsNode() : base(TableAction.NoAction, "Remove Columns")
+    {
+    }
+
+    protected override async Task Invoke(CancellationToken cancellationToken)
+    {
+        var dataTable = GetTable();
+
+        foreach (var column in Columns)
+        {
+            if (!dataTable.Columns.Contains(column))
+            {
+                throw CreateError($"Column '{column}' does not exists");
+            }
+
+            dataTable.Columns.Remove(column);
+        }
+    }
+}
