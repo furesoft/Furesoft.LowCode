2981cb0fb537e9b6e8c079dc074b3da4068171da
Chris <filmee24@gmail.com>
2023-08-29 15:51:36 +0200

    feat: Add New Icons

--- src/Nodes/Furesoft.LowCode.Nodes.RPA.Web/Data/GetTextNode.cs
+++ src/Nodes/Furesoft.LowCode.Nodes.RPA.Web/Data/GetTextNode.cs
@@ -1,9 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
 using Furesoft.LowCode.Nodes.RPA.Web.Core;
+using Furesoft.LowCode.NodeViews;
 
 namespace Furesoft.LowCode.Nodes.RPA.Web.Data;
 
+[NodeView(typeof(IconNodeView),
+    "M0 0 6.0625 0 9 2.9375 9 10.5 0 10.5 0 0ZM1 1 1 9.5 8 9.5 8 4 5 4 5 1 1 1ZM6 1.3125 6 3 7.6875 3 6 1.3125ZM1.5 1.5 4.5 1.5 4.5 2.75 1.5 2.75 1.5 1.5ZM1.5 3.25 4.5 3.25 4.5 4 1.5 4 1.5 3.25ZM1.5 4.5 7.5 4.5 7.5 5.25 1.5 5.25 1.5 4.5ZM1.5 5.75 7.5 5.75 7.5 6.5 1.5 6.5 1.5 5.75ZM1.5 7 7.5 7 7.5 7.75 1.5 7.75 1.5 7ZM1.5 8.25 7.5 8.25 7.5 9 1.5 9 1.5 8.25Z")]
 public class GetTextNode : WebNode, IOutVariableProvider
 {
     public GetTextNode() : base("Get Text")
--- src/Nodes/Furesoft.LowCode.Nodes.RPA.Web/Interaction/TypeIntoNode.cs
+++ src/Nodes/Furesoft.LowCode.Nodes.RPA.Web/Interaction/TypeIntoNode.cs
@@ -8,7 +8,7 @@ namespace Furesoft.LowCode.Nodes.RPA.Web.Interaction;
 
 [NodeView(typeof(IconNodeView),
     Parameter =
-        "M10 3H9V2h1v1zM3 4H2v1h1V4zm5-2H7v1h1V2zM4 2H2v1h2V2zm8 7h2v-1h-2v1zM8 5h1V4H8v1zm-4 3H2v1h2v-1zm8-6h-1v1h1V2zm2 0h-1v1h1V2zm-2 5h2V4h-2v3zm4-6v9c0 .55-.45 1-1 1H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h14c.55 0 1 .45 1 1zm-1 0H1v9h14V1zM6 5h1V4H6v1zm0-3H5v1h1V2zM4 5h1V4H4v1zm1 4h6v-1H5v1zm5-4h1V4h-1v1zM3 6H2v1h1V6zm5 0v1h1V6H8zM6 6v1h1V6H6zM5 6H4v1h1V6zm5 1h1V6h-1v1z")]
+        "M1 16H5.24a1 1 0 00.71-.29l6.92-6.93h0L15.71 6a1 1 0 000-1.42L11.47.29a1 1 0 00-1.42 0L7.23 3.12h0L.29 10.05a1 1 0 00-.29.71V15A1 1 0 001 16ZM10.76 2.41l2.83 2.83L12.17 6.66 9.34 3.83ZM2 11.17l5.93-5.93 2.83 2.83L4.83 14H2Za1 1 0 000 2Z")]
 public class TypeIntoNode : WebNode
 {
     public TypeIntoNode() : base("Type Into")
