4d7be9e5c43c04f73f429d8e3f6756d1fc913634
Chris <filmee24@gmail.com>
2023-08-30 08:36:27 +0200

    refactor: Add LinqNode

--- src/Nodes/Furesoft.LowCode.Nodes.IO.Linq/FilterNode.cs
+++ src/Nodes/Furesoft.LowCode.Nodes.IO.Linq/FilterNode.cs
@@ -1,15 +1,13 @@
 ﻿using System.Collections;
 using System.ComponentModel;
 using System.Runtime.Serialization;
-using Furesoft.LowCode.Attributes;
 using Furesoft.LowCode.Evaluation;
 
 namespace Furesoft.LowCode.Nodes.IO.Linq;
 
 [Description("Filters Nodes")]
-[NodeCategory("Data/Linq")]
 [NodeIcon("M29.8667 64.3333l17.0667-10.6667v-17.0667l29.8667-36.2667h-76.8l29.8667 36.2667z")]
-public class FilterNode : InputOutputNode, IOutVariableProvider, IPipeable
+public class FilterNode : LinqNode
 {
     public FilterNode() : base("Filter")
     {
@@ -19,40 +17,15 @@ public class FilterNode : InputOutputNode, IOutVariableProvider, IPipeable
     [DataMember(IsRequired = false, EmitDefaultValue = false)]
     public Evaluatable<bool> Condition { get; set; }
 
-    [Description("Output Variable")]
-    [DataMember(IsRequired = false, EmitDefaultValue = false)]
-    public string OutVariable { get; set; }
-
-    public object PipeVariable { get; set; }
-
-    public override Task Execute(CancellationToken cancellationToken)
+    protected override IEnumerable Iterate(IEnumerable src)
     {
-        ApplyPipe<IEnumerable>();
-
-        var result = LazyIteratePipeVariable();
-
-        if (!string.IsNullOrEmpty(OutVariable))
+        foreach (var pi in src)
         {
-            SetOutVariable(OutVariable, result);
-        }
+            DefineConstant("$", pi);
 
-        PipeVariable = result;
-
-        return ContinueWith(OutputPin, cancellationToken);
-    }
-
-    public IEnumerable LazyIteratePipeVariable()
-    {
-        if (PipeVariable is IEnumerable p)
-        {
-            foreach (var pi in p)
+            if (Condition)
             {
-                DefineConstant("$", pi);
-
-                if (Condition)
-                {
-                    yield return pi;
-                }
+                yield return pi;
             }
         }
     }
---
+++ src/Nodes/Furesoft.LowCode.Nodes.IO.Linq/LinqNode.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using Furesoft.LowCode.Attributes;
+
+namespace Furesoft.LowCode.Nodes.IO.Linq;
+
+[NodeCategory("Data/Linq")]
+public abstract class LinqNode : InputOutputNode, IOutVariableProvider, IPipeable
+{
+    protected LinqNode(string label) : base(label)
+    {
+    }
+
+    [Description("Output Variable")]
+    [DataMember(IsRequired = false, EmitDefaultValue = false)]
+    public string OutVariable { get; set; }
+
+    public object PipeVariable { get; set; }
+
+    public override Task Execute(CancellationToken cancellationToken)
+    {
+        ApplyPipe<IEnumerable>();
+
+        var result = LazyIteratePipeVariable();
+
+        if (!string.IsNullOrEmpty(OutVariable))
+        {
+            SetOutVariable(OutVariable, result);
+        }
+
+        PipeVariable = result;
+
+        return ContinueWith(OutputPin, cancellationToken);
+    }
+
+    public IEnumerable LazyIteratePipeVariable()
+    {
+        if (PipeVariable is IEnumerable p)
+        {
+            return Iterate(p);
+        }
+
+        return Array.Empty<object>();
+    }
+
+    protected abstract IEnumerable Iterate(IEnumerable src);
+}
