2b1e414ea741ff7cdce5800c06e5bb14d85b53bb
Chris <filmee24@gmail.com>
2023-09-02 12:58:34 +0200

    Update NodeIcon attribute for better visualization
    Modified 'NodeIcon' attribute in four different classes to enhance the visualization of nodes in the network. The change mainly affects 'RestBaseNode', 'ClearNode', 'WaitForNode' and 'ProcessNode' classes in 'Furesoft.LowCode.Nodes' namespace. This update significantly improves user-node interaction in the visual coding interface by providing unique identification patterns for these nodes. The icons reflect the functionality of the respective nodes effectively.

--- src/Nodes/Furesoft.LowCode.Nodes.Network.REST/RESTBaseNode.cs
+++ src/Nodes/Furesoft.LowCode.Nodes.Network.REST/RESTBaseNode.cs
@@ -8,6 +8,8 @@ namespace Furesoft.LowCode.Nodes.Network.REST;
 
 [Description("Send a REST Request To A Server")]
 [NodeCategory("Network/REST")]
+[NodeIcon(
+    "M56 15V9C56 8.4688 55.5313 8 55 8H12V2C12 1.4688 11.5625 1 11 1 10.7188 1 10.4688 1.125 10.25 1.3125L.2813 11.3125C.0938 11.5 0 11.75 0 12 0 12.2813.0938 12.5313.2813 12.7188L10.2813 22.7188C10.4688 22.9063 10.75 23 11 23 11.5313 23 12 22.5625 12 22V16H55C55.5313 16 56 15.5625 56 15ZM56 32C56 31.75 55.9063 31.4688 55.7188 31.2813L45.7188 21.2813C45.5313 21.0938 45.25 21 45 21 44.4688 21 44 21.4688 44 22V28H1C.4688 28 0 28.4688 0 29V35C0 35.5313.4688 36 1 36H44V42C44 42.5625 44.4375 43 45 43 45.2813 43 45.5313 42.875 45.75 42.6875L55.7188 32.7188C55.9063 32.5313 56 32.25 56 32Z")]
 public abstract class RestBaseNode : InputNode, IOutVariableProvider
 {
     protected HttpClient client = new();
@@ -27,9 +29,8 @@ public abstract class RestBaseNode : InputNode, IOutVariableProvider
     public IOutputPin FailurePin { get; set; }
 
     public BindingList<string> Headers { get; set; } = new();
-    
-    [Required]
-    public string OutVariable { get; set; }
+
+    [Required] public string OutVariable { get; set; }
 
     private void ApplyHeaders()
     {
--- src/Nodes/Furesoft.LowCode.Nodes.Queue/ProcessNode.cs
+++ src/Nodes/Furesoft.LowCode.Nodes.Queue/ProcessNode.cs
@@ -5,6 +5,8 @@ using NiL.JS.Core;
 namespace Furesoft.LowCode.Nodes.Queue;
 
 [Description("Process items in a queue")]
+[NodeIcon(
+    "M0 13H3M0 8H3M19 13H22M19 8H22M14 19V22M8 19V22M14 0V3M8 0V3M8 8H14V14H8ZM5 3H17C18.108 3 19 3.892 19 5V17C19 18.108 18.108 19 17 19H5C3.892 19 3 18.108 3 17V5C3 3.892 3.892 3 5 3Z")]
 public class ProcessNode : QueueBaseNode
 {
     public ProcessNode() : base("Process Queue")
